a7ca4f933af7a146aa4b3a72c6ac57ec81314aa0
elderguardian <katzenmaedchen@tutanota.com>
2024-02-05 20:23:41 +0100

    config this

--- main.tsx
+++ main.tsx
@@ -1,20 +1,22 @@
 /** @jsx h */
 
-import blog, { ga, redirects } from "https://deno.land/x/blog@0.7.0/blog.tsx";
+import blog from "https://deno.land/x/blog@0.7.0/blog.tsx";
 
 blog({
-  title: "Hello, world!",
-  description: "Welcome to the Kagerou Blog",
-  avatar: "https://avatars.githubusercontent.com/u/129489839?v=4",
-  favicon: "https://avatars.githubusercontent.com/u/129489839?v=4",
-  avatarClass: "rounded-full",
-  author: "elderguardian",
-  links: [
-    { title: "Email", url: "mailto:kagerou@tuta.io" },
-    { title: "GitHub", url: "https://github.com/elderguardian" },
-  ],
-  lang: "en",
-  dateFormat: (date) =>
-      new Intl.DateTimeFormat("en-GB", { dateStyle: "long" }).format(date),
-  showHeaderOnPostPage: true,
+    title: "Hello, world!",
+    description: "Welcome to the Kagerou Blog",
+    avatar: "https://avatars.githubusercontent.com/u/129489839?v=4",
+    favicon: "https://avatars.githubusercontent.com/u/129489839?v=4",
+    avatarClass: "rounded-full",
+    author: "elderguardian",
+    links: [
+        {title: "Home", url: "https://kagerou.dev"},
+        {title: "Email", url: "mailto:kagerou@tuta.io"},
+        {title: "GitHub", url: "https://github.com/elderguardian"},
+        {title: "Twitter", url: "https://twitter.com/kageroudev"}
+    ],
+    lang: "en",
+    dateFormat: (date) =>
+        new Intl.DateTimeFormat("en-GB", {dateStyle: "long"}).format(date),
+    showHeaderOnPostPage: true,
 });
